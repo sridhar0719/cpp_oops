935300b5ff73be6538328855a9e757a3c0532f5c
sridhar <msridhar@innominds.com>
2024-12-02 15:20:43 +0530

    set_get funcation is modifyed

--- oops/set_get.cpp
+++ oops/set_get.cpp
@@ -1,9 +1,28 @@
 #include<iostream>
 #include<string>
 using namespace std;
-class person{
-	string name;
-	int age;
-	string counter;
 
+class person {
+    private:
+        string name = "sridhar";
+        int age = 24;
+        string counter = "india";
+
+    public:
+        void setPerson(string n, int a, string c) {
+            name = n;
+            age = a;
+            counter = c;
+        }
+        void getPerson() {
+            cout << "Name: " << name << endl;
+            cout << "Age: " << age << endl;
+            cout << "Country: " << counter << endl;
+        }
+};
+
+int main() {
+    person p1;
+    p1.getPerson(); 
+}
 
